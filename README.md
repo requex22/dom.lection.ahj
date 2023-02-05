f6d51280a21fdc0a83cbc94dbcf05dc4e708a73c
Stanislav <stas_sokolov_2001@mail.ru>
2023-02-05 14:57:53 +0600

    1.0

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/m5seajoo66vhp24w?svg=true)](https://ci.appveyor.com/project/requex22/dom-lection-ahj)
+
+# Webpack5
+
+[Информация о релизе Webpack 5](https://webpack.js.org/blog/2020-10-10-webpack-5-release/)
+
+[Migration Guide](https://webpack.js.org/migrate/5/)
---
+++ src/js/app.js
@@ -0,0 +1,46 @@
+function promptBoardSize(){
+  let result = prompt('Введите размер поля')
+
+  if (result < 4){
+    alert('Поле слишком маленькое, введите новое значение');
+    result = prompt('Введите размер поля');
+  }
+
+  return result
+}
+
+function getRandomNumber(min, max){
+  let rand = min + Math.random() * (max - min);
+  return Math.floor(rand);
+}
+
+
+
+
+
+
+const body = document.querySelector('body');
+
+body.insertAdjacentHTML('afterbegin', `
+  <div class="gameBoard"></div>
+`);
+
+const boardSize = promptBoardSize();
+const gameBoard = body.querySelector('.gameBoard');
+
+for (let i = 0; i < boardSize * boardSize; i++){
+  gameBoard.insertAdjacentHTML("afterbegin", `
+    <div class="cell"></div>
+  `)
+}
+
+const cells = gameBoard.querySelectorAll('.cell')
+
+
+gameBoard.style.width = (cells[0].offsetWidth + 20) * boardSize + 'px';
+
+setInterval(() => {
+  cells.forEach(item => item.classList.remove('cellWithGoblin'));
+
+  cells[getRandomNumber(0, cells.length)].classList.add('cellWithGoblin')
+}, 1000);
